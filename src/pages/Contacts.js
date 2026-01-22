03bb2917de98da62c95de6f21eac0c64369de79b
At1anTT <daniil_kiselev@list.ru>
2026-01-22 21:18:58 +0300

    1:08

---
+++ src/pages/Contacts.js
@@ -0,0 +1,25 @@
+function Contacts() {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <p>RU</p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">TG</h2>
+            <p>o_0</p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Email</h2>
+            <p>text@test.ru</p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default Contacts;
